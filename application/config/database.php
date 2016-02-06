5cadaa83826a7665fcfc283e82904103c2816858
ronieri <ronierisales@gmail.com>
2016-02-06 00:12:28 -0200

    Atualização das configurações

--- application/config/database.php
+++ application/config/database.php
@@ -6,7 +6,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'qoakzm';
+$db['default']['password'] = '';
 $db['default']['database'] = 'centro_academico';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
