42d204f7ff7c63a0f2a7edb1178b8beb778595e3
Ronieri Sales <ronieri@ronieri-debian.ronieri-debian>
2016-02-08 13:29:12 -0200

    Adicionado labels ao gráfico de acessos

--- assets/js/dash.js
+++ assets/js/dash.js
@@ -39,5 +39,31 @@ $(function () {
         success: onDataReceived
     });
 
+    $("<div id='tooltip'></div>").css({
+        position: "absolute",
+        display: "none",
+        border: "1px solid #fdd",
+        padding: "2px",
+        "background-color": "#fee",
+        opacity: 0.80
+    }).appendTo("body");
 
+    $("#plot").bind("plothover", function (event, pos, item) {
+
+        if ($("#enablePosition:checked").length > 0) {
+            var str = "(" + pos.x.toFixed(2) + ", " + pos.y.toFixed(2) + ")";
+            $("#hoverdata").text(str);
+        }
+
+        if (item) {
+            var x = item.datapoint[0].toFixed(2),
+                    y = item.datapoint[1].toFixed(2);
+
+            $("#tooltip").html("&nbsp;&nbsp;&nbsp;&nbsp;" + Number(y) + (Number(y) === 1 ? " acesso." : " acessos."))
+                    .css({top: item.pageY + 5, left: item.pageX + 5})
+                    .fadeIn(200);
+        } else {
+            $("#tooltip").hide();
+        }
+    });
 });
